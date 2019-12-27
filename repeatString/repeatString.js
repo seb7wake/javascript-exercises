a1fd73841b9a6814384e0fea7a36b3679e88d818
seb7wake <58760315+seb7wake@users.noreply.github.com>
2019-12-27 14:03:42 -0500

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str1, num) {
+  str2=''
+  for (let i=0;i<num;i++) {
+    str2+=str1;
+  }
+  return str2;
 }
 
 module.exports = repeatString
